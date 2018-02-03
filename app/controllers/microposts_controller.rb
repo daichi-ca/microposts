4377d622eb2d7fa4eb4bf7a621f2b2b8c4ae8f96
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-03 06:39:14 +0000

    correct_userの誤字を修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,18 +8,28 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @micropost = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
   end  
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fall_location: root_path)
   end
   
   private
   
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+  
+  def correct_user
+    @micropost =current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
   end
 end
