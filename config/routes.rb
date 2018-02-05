3ad0a9576c8863a00eada44f3ea2b1ab814c30c1
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-05 00:31:28 +0000

    destroyのスペルミスを訂正

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   end
   
   resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destory]
+  resources :relationships, only: [:create, :destroy]
 end
